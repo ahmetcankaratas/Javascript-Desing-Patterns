145050ca681663f247448a317d0a2869475896c3
lingardium <karatascanahmet@yahoo.com>
2022-04-03 14:25:34 +0000

    requirement #2 - Two Cats

--- cat-clicker/script.js
+++ cat-clicker/script.js
@@ -1,3 +1,4 @@
+// First Cat
 var elem = document.getElementById('my-elem');
 var count = document.getElementById('count');
 var clickedNumber = 0;
@@ -7,3 +8,12 @@ elem.addEventListener('click', function(){
     count.innerHTML = clickedNumber
   }, false);
 
+//Second Cat
+var elem2 = document.getElementById('my-elem2');
+var count2 = document.getElementById('count2');
+var clickedNumber2 = 0;
+
+elem2.addEventListener('click', function(){
+    clickedNumber2 += 1
+    count2.innerHTML = clickedNumber2
+  }, false);
